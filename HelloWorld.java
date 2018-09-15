d4d4c5f3056ad70eea31ccc862cbf67de02239bd
vlu00 <vlu00@stuy.edu>
2018-09-14 20:04:10 -0400

    Wrote java program that outputs a greeting.

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello my name is Vivien, and I can't wait to learn more about computer science!");
+    }
+}
